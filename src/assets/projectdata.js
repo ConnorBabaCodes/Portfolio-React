951cd2223c3eb6776c169aa2e047caa10535f885
connor <connorjbaba@gmail.com>
2022-09-08 08:21:42 -0400

    added dynamic project generation in projects component

---
+++ src/assets/projectdata.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        id: 1,
+        name: "Project 1",
+        description: "Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam",
+        tech: ["sass, typeScript, react"],
+        github: "asdf",
+        demo: "fdsa",
+        imageUrl: "test"
+
+    },
+    {
+        id: 2,
+        name: "Project 2",
+        description: "Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam",
+        tech: ["sass, typeScript, react"],
+        github: "asdf",
+        demo: "fdsa",
+        imageUrl: "test"
+
+    },
+    {
+        id: 3,
+        name: "Project 3",
+        description: "Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam",
+        tech: ["sass, typeScript, react"],
+        github: "asdf",
+        demo: "fdsa",
+        imageUrl: "test"
+
+    }
+]
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
+import projectData from '/src/assets/projectdata.js'
+
+
+
 
 export default function Projects() {
+    const projectElements = projectData.map(project => {
+        return <div>
+                <h2>
+                    {project.name}
+                    </h2>
+                <p>
+                    {project.description}
+                    </p>
+                <p>
+                    {project.tech}
+                    </p>
+                <div>
+                    {project.github}{project.demo}
+                </div>
+        </div> 
+    })
+    
     return (
-        <div></div>
+        <section>
+            <h1>PROJECTS</h1>
+            <div>{projectElements}</div>
+        
+        </section>
     )
 }

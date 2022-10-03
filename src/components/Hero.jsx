61d56e0512b239a3e2c2a1382ad9826f8ef39d23
connor <connorjbaba@gmail.com>
2022-10-02 21:35:08 -0400

    font size and spacing adjustments

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ export default function Hero() {
     return (
         <div id="hero">
             <h1>Hi, I'm <span className="name">Connor Baba.</span></h1>
-            <h2>A Front End Engineer.</h2>
+            <h2>A Web Developer.</h2>
             <p>I work with <span className="HTML">HTML</span> <span className="CSS">CSS</span> <span className="js">Javascript</span> and <span className="react">React</span> to deliver responsive web technologies that are clean, safe, and scaleable. Let's build a project!</p>
                 <div className="hero-links">
                     <ul>
